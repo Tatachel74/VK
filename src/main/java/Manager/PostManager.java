85d23c59a8e36797892916c78ff1db61be530488
Tanya <tanya.butova@gmail.com>
2021-01-20 18:38:46 +0500

    PostManager

---
+++ src/main/java/Manager/PostManager.java
@@ -0,0 +1,12 @@
+package Manager;
+
+import domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] search(int ownerId, String domain, String query, int ownersOnly, int count, int offset){
+        return null;
+    }
+    public void delete(int ownerId, int postId){
+    }
+}
